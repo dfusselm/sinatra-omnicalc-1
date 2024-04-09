c97f51421fbf0cb5ab189e735a83f43f19284a09
dfusselm <165109847+dfusselm@users.noreply.github.com>
2024-04-09 16:21:28 +0000

    attempt 1

--- app.rb
+++ app.rb
@@ -38,3 +38,31 @@ get("/square_root/results") do
 
   erb(:square_root_results)
 end
+
+
+get("/payment/results") do
+  @apr = params.fetch("user_apr").to_f
+  @years = params.fetch("user_years").to_f
+  @pv = params.fetch("user_pv").to_f
+
+  @r = @apr / 100 / 12
+  @denominator = 1- ((1+@r) ** (-@years * 12))
+  @numerator = @r * @pv
+  @p = @numerator / @denominator
+  @apr = @apr.to_fs(:percentage, {:precision => 4})
+  @pv = @pv.to_fs(:currency)
+  @p = @p.to_fs(:currency)
+  
+  
+
+  erb(:payment_results)
+end
+
+get("/random/results") do
+  @min = params.fetch("user_min").to_f
+  @max = params.fetch("user_max").to_f
+
+  @result = rand(@min..@max)
+
+  erb(:random_results)
+end
